22de5994f02422f0ed44c72e9317dd369fd37de2
Matthew <matthew.robert.leroe@gmail.com>
2020-03-09 19:43:12 -0400

    Nothing created yet.

---
+++ src/test/java/com/example/javascripttojavapractice/JavascripttojavapracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascripttojavapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascripttojavapracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
